32d60479573750a2480c40da8a30ff007fbcc94a
Bogdan Bondarenko <77720392+BogdanBon@users.noreply.github.com>
2021-08-07 14:18:46 +0200

    basics

---
+++ src/js/main-content.js
@@ -0,0 +1,10 @@
+import MovieService from './movie-service';
+import movieTpl from '../templates/cards.hbs'
+
+const newMovieService = new MovieService();
+
+function onSearch() {
+    return newMovieService.fetchMovies()
+}
+
+onSearch()
