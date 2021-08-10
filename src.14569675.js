774689d5ea4c8fb242bcd0f2ea9db95285dfc803
BogdanBon <77720392+BogdanBon@users.noreply.github.com>
2021-08-10 08:43:25 +0000

    Deploying to gh-pages from @ BogdanBon/team-project-JS@90d6500ac3389b9202a1776ff706ae1326df5b12 🚀

--- src.14569675.js
+++ src.14569675.js
@@ -96,4 +96,4 @@ module.exports=function(t){return fetch(t).then(function(t){return t.text()})};
 },{}],0:[function(require,module,exports) {
 var b=require("TUK3");b.register("html",require("A3BY"));b.load([["main-content.0160b0f9.html","BUSN"]]).then(function(){require("Focm");});
 },{}]},{},[0], null)
-//# sourceMappingURL=/team-project-JS/src.22e69b13.js.map
+//# sourceMappingURL=/team-project-JS/src.14569675.js.map
