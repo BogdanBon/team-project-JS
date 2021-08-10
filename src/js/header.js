80d9a994fa9f354d174e8d82ac8e71c9fc578cfc
halyna-ala <77680693+halyna-ala@users.noreply.github.com>
2021-08-11 00:32:09 +0200

    button-que

---
+++ src/js/header.js
@@ -0,0 +1,21 @@
+// import
+
+
+
+
+const btnWatchedEl = document.querySelector('.js-btn-watched');
+    const btnQueueEl = document.querySelector('.js-btn-queue')
+
+
+      btnWatchedEl.addEventListener("click", checkWatchedList);
+    btnQueueEl.addEventListener("click", checkQueueList);
+    
+//     function checkWatchedList() {
+    
+//   }
+
+//   function checkQueueList() {
+   
+//   }
+
+  
