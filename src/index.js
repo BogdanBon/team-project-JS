6173641b6127e3509dfa8c538ef8f0d8d32e17b6
Tetiana Shevchuk <shevchuktetiana4@gmail.com>
2021-08-11 19:53:39 +0200

    add code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import './sass/main.scss';
 import './js/fetch-by-query';
 import './js/loading-popular-films-on-start-page';
 import './js/fetch-by-one-card';
-import './js/pagination';
+
 import './js/add-to-local-storage';
 import './js/variables';
