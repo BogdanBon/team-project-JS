30c9ceb82c0e8267277ed4e0c65fffedff9bf1cb
AlexRosly <rosly.a777@gmail.com>
2021-08-11 19:46:02 +0300

    Добавлена обработка хедера
    Динамическое изменение хедера при нажатии на кнопки: домой, библиотека и нажатии на лого

---
+++ src/js/header.js
@@ -0,0 +1,41 @@
+const refs = {
+  siteLogo: document.querySelector('.site-nav__logo'),
+  navigationBtn: document.querySelector('.site-nav__list'),
+  homeBtn: document.querySelector('.home-js'),
+  libBtn: document.querySelector('.library-js'),
+  form: document.querySelector('.form'),
+  notification: document.querySelector('.notification'),
+  overlayBtn: document.querySelector('.overlay'),
+  header: document.querySelector('.header'),
+};
+
+refs.siteLogo.addEventListener('click', openMainPage);
+refs.homeBtn.addEventListener('click', changeHomePage);
+refs.libBtn.addEventListener('click', changeLibraryPage);
+refs.siteLogo.addEventListener('click', onLogoClick);
+
+function changeLibraryPage(e) {
+  refs.form.classList.remove('is-visible');
+  refs.notification.classList.remove('is-visible');
+  refs.overlayBtn.classList.replace('transform', 'is-visible');
+  refs.homeBtn.classList.remove('site-nav__item--current');
+  refs.libBtn.classList.add('site-nav__item--current');
+  refs.header.classList.replace('imgHome', 'imgLibrary');
+}
+
+function changeHomePage(e) {
+  refs.overlayBtn.classList.replace('is-visible', 'transform');
+  refs.form.classList.add('is-visible');
+  refs.notification.classList.add('is-visible');
+  refs.libBtn.classList.remove('site-nav__item--current');
+  refs.homeBtn.classList.add('site-nav__item--current');
+  refs.header.classList.replace('imgLibrary', 'imgHome');
+}
+
+function openMainPage(e) {
+  changeHomePage();
+}
+
+function onLogoClick(e) {
+  changeHomePage();
+}
