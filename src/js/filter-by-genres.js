35aeb11dff35bec7947739c38049f5b52c8b0fa0
mashtalerleonid <mashtalerleonid@gmail.com>
2021-08-17 14:35:43 +0300

    progress-3

--- src/js/filter-by-genres.js
+++ src/js/filter-by-genres.js
@@ -1,19 +1,22 @@
 import MovieApiService from './movie-service';
 import { fetchQuery } from './fetch-by-query';
 
+let checkedGenresArr = [];
+
 const paginationContainer = document.querySelector('#tui-pagination-container');
 
 const movieApiService = new MovieApiService(paginationContainer.dataset.fetchtype);
 
 const genreBtn = document.querySelector('.genres__container');
 
-genreBtn.addEventListener('click', onHendleBtn);
+genreBtn.addEventListener('change', onHendleBtn);
 
 function onHendleBtn(e) {
-  const checkedGenre = e.target.dataset.genre;
+  // const checkedGenre = e.target.dataset.genre;
   const ppage = 7;
+  checkedGenresArr.push(e.target.value);
   movieApiService.url = paginationContainer.dataset.fetchtype;
   movieApiService.page = 7;
-  fetchQuery(movieApiService);
-  console.log(movieApiService.url);
+  // fetchQuery(movieApiService);
+  console.log(checkedGenresArr);
 }
